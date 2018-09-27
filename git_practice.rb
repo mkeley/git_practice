bfa1c646e56af4e68900addb8f9348db0455dd1f
mkeley <43392279+mkeley@users.noreply.github.com>
2018-09-26 22:50:36 -0400

    Add files via upload

---
+++ git_practice.rb
@@ -0,0 +1,19 @@
+puts "My Name is Mike"
+puts
+puts "I like Pizza"
+puts
+puts "When is lunch?"
+puts
+print "What's your first name? "
+first_name = gets.chomp
+
+print "What's your last name? "
+last_name = gets.chomp
+
+print "What city are you from? "
+city = gets.chomp
+
+print "What state or province are you from? "
+state = gets.chomp
+
+puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
